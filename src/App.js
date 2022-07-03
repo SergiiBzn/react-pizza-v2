1f592eebaf18acff29a14dc46c59ad10f30307db
Kostiantyn Rakhmanov <rahmanoff@gmail.com>
2022-07-03 21:49:16 +0300

    Components: Header, Cat, Sort, PizzaBlock, props

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
+import './scss/app.scss';
+import Header from './components/Header';
+import Categories from './components/Categories';
+import Sort from './components/Sort';
+import PizzaBlock from './components/PizzaBlock';
+
 function App() {
   return (
-    <div className="App">
-      <h1>React Pizza v.2</h1>
+    <div class="wrapper">
+      <Header />
+      <div class="content">
+        <div class="container">
+          <div class="content__top">
+            <Categories />
+            <Sort />
+          </div>
+          <h2 class="content__title">Все пиццы</h2>
+          <div class="content__items">
+            <PizzaBlock title="Мексиканская" price={35} imageUrl="" />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
