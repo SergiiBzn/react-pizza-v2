9f128668ce3cd91bc7faeace0316a2cd9586b1c7
Kostiantyn Rakhmanov <rahmanoff@gmail.com>
2022-07-06 16:00:57 +0300

    logo, Category, json, size, type

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,11 @@
+import logoSvg from '../assets/img/pizza-logo.svg'
+
 function Header() {
     return (
       <div className="header">
         <div className="container">
           <div className="header__logo">
-            <img width="38" src="./img/pizza-logo.svg" alt="Pizza logo" />
+            <img width="38" src={logoSvg} alt="Pizza logo" />
             <div>
               <h1>React Pizza</h1>
               <p>самая вкусная пицца во вселенной</p>
@@ -11,7 +13,7 @@ function Header() {
           </div>
           <div className="header__cart">
             <a href="/cart.html" className="button button--cart">
-              <span>520 ₽</span>
+              <span>52 $</span>
               <div className="button__delimiter"></div>
               <svg
                 width="18"
