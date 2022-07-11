eba8d16bd45cb4127e740b0ba16a84ae56ad0ebe
Kostiantyn Rakhmanov <rahmanoff@gmail.com>
2022-07-11 14:38:08 +0300

    Grid, Responsive, Cart List, ScrollTo

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react';
 
 import Categories from '../components/Categories';
 import Sort from '../components/Sort';
@@ -6,8 +6,8 @@ import PizzaBlock from '../components/PizzaBlock';
 import Skeleton from '../components/PizzaBlock/Skeleton';
 
 const Home = () => {
-    const [items, setItems] = React.useState([]);
-    const [isLoading, setIsLoading] = React.useState(true);
+  const [items, setItems] = React.useState([]);
+  const [isLoading, setIsLoading] = React.useState(true);
 
   React.useEffect(() => {
     fetch('https://62c6a6802b03e73a58d31660.mockapi.io/items')
@@ -16,31 +16,32 @@ const Home = () => {
         setItems(arr);
         setIsLoading(false);
       });
+    window.scrollTo(0, 0);
   }, []);
 
-    return (
-        <>
-        <div className="content__top">
-            <Categories />
-            <Sort />
-        </div>
-        <h2 className="content__title">Все пиццы</h2>
-        <div className="content__items">
-            {isLoading
-            ? [...new Array(6)].map((_, index) => <Skeleton key={index} />)
-            : items.map((obj) => (
-                <PizzaBlock
-                    key={obj.id}
-                    title={obj.title}
-                    price={obj.price}
-                    imageUrl={obj.imageUrl}
-                    sizes={obj.sizes}
-                    types={obj.types}
-                  />
-                ))}
-          </div>
-        </>
-    );
+  return (
+    <div className="container">
+      <div className="content__top">
+        <Categories />
+        <Sort />
+      </div>
+      <h2 className="content__title">Все пиццы</h2>
+      <div className="content__items">
+        {isLoading
+          ? [...new Array(6)].map((_, index) => <Skeleton key={index} />)
+          : items.map((obj) => (
+              <PizzaBlock
+                key={obj.id}
+                title={obj.title}
+                price={obj.price}
+                imageUrl={obj.imageUrl}
+                sizes={obj.sizes}
+                types={obj.types}
+              />
+            ))}
+      </div>
+    </div>
+  );
 };
 
 export default Home;
