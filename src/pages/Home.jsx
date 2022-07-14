8b7e0c73240e096b19198fbe381c7bd381416e6b
Kostiantyn Rakhmanov <rahmanoff@gmail.com>
2022-07-14 13:11:40 +0300

    Controled Input, Search non(&BackEnd), Pagination

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,8 +4,9 @@ import Categories from '../components/Categories';
 import Sort from '../components/Sort';
 import PizzaBlock from '../components/PizzaBlock';
 import Skeleton from '../components/PizzaBlock/Skeleton';
+import Pagination from '../components/Pagination';
 
-const Home = () => {
+const Home = ({ searchValue }) => {
   const [items, setItems] = React.useState([]);
   const [isLoading, setIsLoading] = React.useState(true);
   const [categoryId, setCategoryId] = React.useState(0);
@@ -13,15 +14,17 @@ const Home = () => {
     name: 'популярности',
     sortProperty: 'rating',
   });
+  const [currentPage, setCurrentPage] = React.useState(1);
 
   const sortBy = sortType.sortProperty.replace('-', '');
   const order = sortType.sortProperty.includes('-') ? 'asc' : 'desc';
   const category = categoryId > 0 ? `category=${categoryId}` : '';
+  const search = searchValue ? `&search=${searchValue}` : '';
 
   React.useEffect(() => {
     setIsLoading(true);
     fetch(
-      `https://62c6a6802b03e73a58d31660.mockapi.io/items?${category}&sortBy=${sortBy}&order=${order}`,
+      `https://62c6a6802b03e73a58d31660.mockapi.io/items?page=${currentPage}&limit=4&${category}&sortBy=${sortBy}&order=${order}${search}`,
     )
       .then((res) => res.json())
       .then((arr) => {
@@ -29,7 +32,27 @@ const Home = () => {
         setIsLoading(false);
       });
     window.scrollTo(0, 0);
-  }, [categoryId, sortType]);
+  }, [categoryId, sortType, searchValue, currentPage]);
+
+  const pizzas = items
+    // .filter((obj) => {
+    //   if (obj.title.toLowerCase().includes(searchValue.toLowerCase())) {
+    //     return true;
+    //   }
+    //   return false;
+    // })
+    .map((obj) => (
+      <PizzaBlock
+        key={obj.id}
+        title={obj.title}
+        price={obj.price}
+        imageUrl={obj.imageUrl}
+        sizes={obj.sizes}
+        types={obj.types}
+      />
+    ));
+
+  const skeletons = [...new Array(4)].map((_, index) => <Skeleton key={index} />);
 
   return (
     <div className="container">
@@ -39,20 +62,8 @@ const Home = () => {
         <Sort value={sortType} onChangeSort={(i) => setSortType(i)} />
       </div>
       <h2 className="content__title">Все пиццы</h2>
-      <div className="content__items">
-        {isLoading
-          ? [...new Array(4)].map((_, index) => <Skeleton key={index} />)
-          : items.map((obj) => (
-              <PizzaBlock
-                key={obj.id}
-                title={obj.title}
-                price={obj.price}
-                imageUrl={obj.imageUrl}
-                sizes={obj.sizes}
-                types={obj.types}
-              />
-            ))}
-      </div>
+      <div className="content__items">{isLoading ? skeletons : pizzas}</div>
+      <Pagination onChangePage={(number) => setCurrentPage(number)} />
     </div>
   );
 };
