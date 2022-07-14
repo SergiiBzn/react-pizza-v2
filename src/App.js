099c93a5ac5c925f1e72a40d0392a971c00f88dc
Kostiantyn Rakhmanov <rahmanoff@gmail.com>
2022-07-14 17:17:33 +0300

    React-Redux Toolkit, Reducer

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+import { decrement, increment } from './redux/slices/filterSlice';
+
 import './scss/app.scss';
 import Header from './components/Header';
 import Home from './pages/Home';
 import NotFound from './pages/NotFound';
-import { Routes, Route } from 'react-router-dom';
 import Cart from './pages/Cart';
 
 export const SearchContext = React.createContext('');
 
 function App() {
   const [searchValue, setSearchValue] = React.useState('');
+
+  const count = useSelector((state) => state.counter.count);
+  const dispatch = useDispatch();
 
   return (
     <div className="wrapper">
